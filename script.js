bc4245b6fff6f2dc480dfaf137bdb7958c1b28e7
Devesh Goswami <53081430+devesh297@users.noreply.github.com>
2025-03-27 07:58:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const line = document.getElementById("line");
+let angle = 0;
+
+
+function rotateLine() {
+    angle += 2; 
+    line.style.transform = `rotate(${angle}deg) translate(-50%, -50%)`;
+}
+setInterval(rotateLine, 20);
