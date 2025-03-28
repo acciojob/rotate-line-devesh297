2a91dd9632f660dadf5c9e91f6d3c85afed4922f
Devesh Goswami <53081430+devesh297@users.noreply.github.com>
2025-03-29 01:03:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 const line = document.getElementById("line");
 let angle = 0;
 
-function rotateLine() {
-    angle += 2; // Increase rotation angle
-    line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
-
-   
-    console.log(`Rotation: ${angle}deg`);
-}
-
-// Rotate smoothly every 20 milliseconds
-setInterval(rotateLine, 20);
+setInterval(() => {
+    angle += 2;
+    line.style.transform = translate(-50%, -50%) rotate(${angle}deg);
+}, 20);
